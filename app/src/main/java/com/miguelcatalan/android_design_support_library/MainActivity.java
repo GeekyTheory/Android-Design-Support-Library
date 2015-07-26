768c792d9f1403befb0b2fe336ffe1dc61f6e2b0
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2015-07-26 13:56:46 +0200

    Android Design Support Library (II): FloatActionButton

--- app/src/main/java/com/miguelcatalan/android_design_support_library/MainActivity.java
+++ app/src/main/java/com/miguelcatalan/android_design_support_library/MainActivity.java
@@ -1,23 +1,34 @@
 package com.miguelcatalan.android_design_support_library;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setupToolbar();
-
+        setupFAB();
     }
 
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private void setupFAB() {
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Do something cool
+            }
+        });
     }
 }
