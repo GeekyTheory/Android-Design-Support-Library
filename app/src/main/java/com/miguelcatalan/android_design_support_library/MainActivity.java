acc48bc6b30986925cf0e7cd713ba01f43e8f80a
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2015-08-04 10:31:28 +0200

    Android Design Support Library (III): NavigationView

--- app/src/main/java/com/miguelcatalan/android_design_support_library/MainActivity.java
+++ app/src/main/java/com/miguelcatalan/android_design_support_library/MainActivity.java
@@ -2,17 +2,25 @@ package com.miguelcatalan.android_design_support_library;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupNavigationView();
         setupToolbar();
         setupFAB();
     }
@@ -20,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_navigation_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     private void setupFAB() {
@@ -30,5 +43,20 @@ public class MainActivity extends AppCompatActivity {
                 //Do something cool
             }
         });
+    }
+
+    private void setupNavigationView(){
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
