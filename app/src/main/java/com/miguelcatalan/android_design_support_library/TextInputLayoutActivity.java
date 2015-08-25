009e0277a616672dc54fd35c7991a749695231a1
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2015-08-25 13:40:16 +0200

    Android Design Support Library (VI): TextInputLayout

--- app/src/main/java/com/miguelcatalan/android_design_support_library/TextInputLayoutActivity.java
+++ app/src/main/java/com/miguelcatalan/android_design_support_library/TextInputLayoutActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,22 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+public class TextInputLayoutActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    TabLayout tabLayout;
-    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_text_input_layout);
 
         setupNavigationView();
         setupToolbar();
-        setupTabLayout();
-        setupFAB();
     }
 
     private void setupToolbar() {
@@ -41,11 +35,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_navigation_menu);
         ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    private void setupFAB() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(this);
     }
 
     private void setupNavigationView() {
@@ -60,18 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void setupTabLayout() {
-
-        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setTabMode(TabLayout.MODE_FIXED);
-
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
-
-        tabLayout.setupWithViewPager(viewPager);
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -80,17 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.fab) {
-
-            Snackbar
-                    .make(findViewById(R.id.container), "This is Snackbar", Snackbar.LENGTH_LONG)
-                    .setAction("Action", this) //To add additional action when clicked
-                    .show(); // Don’t forget to show!
-        }
     }
 
     @Override
